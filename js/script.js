8a5a26a07b0541575b17edeed27ee329fdbf7d44
Michael-Mbaya <gikundamike@gmail.com>
2020-07-25 18:21:20 +0300

    jquery effects: css, toggle & fadeToggle

---
+++ js/script.js
@@ -0,0 +1,101 @@
+$(document).ready(function(){
+    $(".row").css({"text-align":"center"});
+    $(".h-hide").css({"font-weight":"bold","text-transform":"uppercase"});  
+});
+
+$(document).ready(function() {
+    $(".img-do1").click(function() {
+      $(".describe1").slideToggle();
+      $(".img-do1").slideToggle();
+    });
+    $(".describe1").click(function() {
+        $(".img-do1").slideToggle();
+        $(".describe1").slideToggle();
+    }); 
+});
+
+$(document).ready(function() {
+    $(".img-do2").click(function() {
+      $(".describe2").slideToggle();
+      $(".img-do2").slideToggle();
+    });
+    $(".describe2").click(function() {
+        $(".img-do2").slideToggle();
+        $(".describe2").slideToggle();
+    }); 
+});
+
+$(document).ready(function() {
+    $(".img-do3").click(function() {
+      $(".describe3").slideToggle();
+      $(".img-do3").slideToggle();
+    });
+    $(".describe3").click(function() {
+        $(".img-do3").slideToggle();
+        $(".describe3").slideToggle();
+    }); 
+});
+
+$(document).ready(function(){
+  $('#hover1').hover(function(){
+    $('#hovershow1').fadeToggle(750);
+    }, function(){
+    $('#hovershow1').fadeToggle(500);
+  });
+});
+
+$(document).ready(function(){
+  $("#hover2").hover(function(){
+    $("#hovershow2").fadeToggle(750);
+    }, function(){
+    $("#hovershow2").fadeToggle(500);
+  });
+});
+
+$(document).ready(function(){
+  $("#hover3").hover(function(){
+    $("#hovershow3").fadeToggle(750);
+    }, function(){
+    $("#hovershow3").fadeToggle(500);
+  });
+});
+
+$(document).ready(function(){
+  $("#hover4").hover(function(){
+    $("#hovershow4").fadeToggle(750);
+    }, function(){
+    $("#hovershow4").fadeToggle(500);
+  });
+});
+
+$(document).ready(function(){
+    $("#hover5").hover(function(){
+      $("#hovershow5").fadeToggle(750);
+      }, function(){
+      $("#hovershow5").fadeToggle(500);
+    });
+  });
+
+  $(document).ready(function(){
+    $("#hover6").hover(function(){
+      $("#hovershow6").fadeToggle(750);
+      }, function(){
+      $("#hovershow6").fadeToggle(500);
+    });
+  });
+
+  $(document).ready(function(){
+    $("#hover7").hover(function(){
+      $("#hovershow7").fadeToggle(750);
+      }, function(){
+      $("#hovershow7").fadeToggle(500);
+    });
+  });
+
+  $(document).ready(function(){
+    $("#hover8").hover(function(){
+      $("#hovershow8").fadeToggle(750);
+      }, function(){
+      $("#hovershow8").fadeToggle(500);
+    });
+  });
